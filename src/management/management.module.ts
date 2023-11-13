5657936c3ffc2b7863d9097d902dc5bfaf9397ff
MILODIAZ <milodiaz1995@gmail.com>
2023-11-13 20:13:52 -0300

    proyectos

--- src/management/management.module.ts
+++ src/management/management.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TeamController } from './team/controller/team.controller';
 import { ProxyModule } from 'src/common/proxy/proxy.module';
 import { ClientProxyNotJira } from 'src/common/proxy/client-proxy';
+import { ProjectController } from './project/controller/project/project.controller';
 
 @Module({
   imports: [ProxyModule],
   providers: [ClientProxyNotJira],
-  controllers: [TeamController],
+  controllers: [TeamController, ProjectController],
 })
 export class ManagementModule {}
---
+++ src/management/project/controller/project/project.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { ApiTags } from '@nestjs/swagger';
+import { ProjectMSG } from 'src/common/constants';
+import { projectDto } from '../../dto/project.dto';
+import { ClientProxyNotJira } from 'src/common/proxy/client-proxy';
+
+@ApiTags('projects')
+@Controller('api/v1/project')
+export class ProjectController {
+  constructor(private readonly clientProxy: ClientProxyNotJira) {}
+  private clientProxyManagement = this.clientProxy.clientProxyManagement();
+
+  @Get()
+  findAll() {
+    return this.clientProxyManagement.send(ProjectMSG.FIND_ALL, '');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.clientProxyManagement.send(ProjectMSG.FIND_ONE, id);
+  }
+
+  @Post()
+  create(@Body() payload: projectDto) {
+    return this.clientProxyManagement.send(ProjectMSG.CREATE, payload);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: projectDto) {
+    return this.clientProxyManagement.send(ProjectMSG.UPDATE, { id, payload });
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.clientProxyManagement.send(ProjectMSG.DELETE, id);
+  }
+}
